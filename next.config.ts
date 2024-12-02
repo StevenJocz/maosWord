30492976b27fb32f234ac9e814eea3f5a5da5865
root <steven-jocz@hotmail.com>
2024-12-01 20:53:21 -0500

    Ajustes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  ouput: "export",
+  distDir: "dist",
   images: {
+    unoptimized: true,
     domains: ['controleur.ca', 'img.freepik.com', 'th.bing.com'], // Agrega el dominio de tu imagen
   },
 };
